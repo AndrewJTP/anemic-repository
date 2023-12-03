ec603e9b3f95907d440d0c42d1771f37ba5b1d7c
AndrewTorrez <andrew.j.t.p@gmail.com>
2023-12-02 22:28:03 -0400

    test2

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def allowSelfSignedHttps(allowed):
     # bypass the server certificate verification on client side
     if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
         ssl._create_default_https_context = ssl._create_unverified_context
-allowSelfSignedHttps(True) 
+allowSelfSignedHttps(True)
 
 app= Flask(__name__)
 CORS(app, origins='*')
@@ -23,22 +23,22 @@ def root():
 
 @app.route('/predict', methods=['POST'])
 def predic_anemia_level():
-    data =request.get_json()
+    data = request.get_json()
 
     body = str.encode(json.dumps(data))
     url = 'https://proy-children-anemic-ugnzw.eastus2.inference.ml.azure.com/score'
-    api_key = 'Fi7RPPXhlgofRGZxxRx4M9ShOfzeAe9H'
+    api_key = '4LdIDdrOhpJvZDCbT0hcFi7HMHgDU3JE'
     if not api_key:
         return jsonify({"error":"A key should be provided to invoke the endpoint"}), 404
-    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'anemic-prediction-model-1' }
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'goofyeyeq8s3jf140-1' }
     req = urllib.request.Request(url, body, headers)
-    # try:
-    #     response = urllib.request.urlopen(req)
-    #     result = response.read()
-    #     return result
-    # except urllib.error.HTTPError as error:
-    #     return jsonify({"error":"erorrrrrrrr"}), 404
-    return jsonify({"result":"Not Anemic"}), 200
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read()
+        return result
+    except urllib.error.HTTPError as error:
+        return jsonify({"error":error}), 404
+    #return jsonify({"result":"Not Anemic"}), 200
 
 if __name__ == "__main__":
     app.run()
